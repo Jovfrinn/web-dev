3b42de90048f0e49f448e7890d2b7a10beb2e4bb
dzaky <dzakyr1610@gmail.com>
2024-08-24 13:10:17 +0700

    halaman makanan dan minuman

---
+++ resources/views/fronsite/halaman-makanan/makanan-instan.blade.php
@@ -0,0 +1,100 @@
+@extends('fronsite.layouts.navbar')
+
+@section('food-content')
+<div class="container-beranda">
+    <div class="nopad-beranda">
+        <a href="#" class="link-beranda">Beranda</a><span class="material-symbols-outlined" style="color:white">
+            arrow_forward
+            </span>
+            <p class="p-page">Halaman Makanan</p>
+    </div>
+</div>
+<div class="sidebar">
+    <a href="{{ route('category-snack') }}">Snack</a>
+    <a href="{{ route('category-kalengan') }}">Makanan Kalengan</a>
+    <a href="{{ route('category-biskuit') }}">Roti/Biskuit</a>
+    <a class="active" {{ Route::currentRouteName() == 'category-instan' ? 'active' : '' }} href="{{ route('category-instan') }}">Makanan Instan</a>
+    <a href="{{ route('category-manis') }}">Makanan Manis</a>
+  </div>
+
+<div class="container">
+    <div class="content">
+        <div class="title-content">Makanan Instan</div>
+        <div class="list slider-slick">
+            <div class="card-list d-flex flex-column align-items-center">
+                <img src="https://www.sunlight.co.id/images/h0nadbhvm6m4/1VLa8YgpNnTTHS8hVcRpFx/f877e731e3474ef8f0b991083e3d69a1/U3VubGlnaHRfSmVydWtfTmlwaXMxLnBuZw/1080w-1080h/sunlight-jeruk-nipis.jpg" alt="">
+                <div class="title-produk">sunlight</div>
+                <div class="price">Rp 21.500</div>
+                </a>
+                <button class="btn cart-btn"><span class="material-symbols-outlined">
+                    add
+                    </span>Keranjang</button>
+            </div>
+            <div class="card-list d-flex flex-column align-items-center">
+                <img src="https://www.sunlight.co.id/images/h0nadbhvm6m4/1VLa8YgpNnTTHS8hVcRpFx/f877e731e3474ef8f0b991083e3d69a1/U3VubGlnaHRfSmVydWtfTmlwaXMxLnBuZw/1080w-1080h/sunlight-jeruk-nipis.jpg" alt="">
+                <div class="title-produk">sunlight</div>
+                <div class="price">Rp 21.500</div>
+                </a>
+                <button class="btn cart-btn"><span class="material-symbols-outlined">
+                    add
+                    </span>Keranjang</button>
+            </div>
+            <div class="card-list d-flex flex-column align-items-center">
+                <img src="https://www.sunlight.co.id/images/h0nadbhvm6m4/1VLa8YgpNnTTHS8hVcRpFx/f877e731e3474ef8f0b991083e3d69a1/U3VubGlnaHRfSmVydWtfTmlwaXMxLnBuZw/1080w-1080h/sunlight-jeruk-nipis.jpg" alt="">
+                <div class="title-produk">sunlight</div>
+                <div class="price">Rp 21.500</div>
+                </a>
+                <button class="btn cart-btn"><span class="material-symbols-outlined">
+                    add
+                    </span>Keranjang</button>
+            </div>
+            <div class="card-list d-flex flex-column align-items-center">
+                <img src="https://www.sunlight.co.id/images/h0nadbhvm6m4/1VLa8YgpNnTTHS8hVcRpFx/f877e731e3474ef8f0b991083e3d69a1/U3VubGlnaHRfSmVydWtfTmlwaXMxLnBuZw/1080w-1080h/sunlight-jeruk-nipis.jpg" alt="">
+                <div class="title-produk">sunlight</div>
+                <div class="price">Rp 21.500</div>
+                </a>
+                <button class="btn cart-btn"><span class="material-symbols-outlined">
+                    add
+                    </span>Keranjang</button>
+            </div>
+            <div class="card-list d-flex flex-column align-items-center">
+                <img src="https://www.sunlight.co.id/images/h0nadbhvm6m4/1VLa8YgpNnTTHS8hVcRpFx/f877e731e3474ef8f0b991083e3d69a1/U3VubGlnaHRfSmVydWtfTmlwaXMxLnBuZw/1080w-1080h/sunlight-jeruk-nipis.jpg" alt="">
+                <div class="title-produk">sunlight</div>
+                <div class="price">Rp 21.500</div>
+                </a>
+                <button class="btn cart-btn"><span class="material-symbols-outlined">
+                    add
+                    </span>Keranjang</button>
+            </div>
+            <div class="card-list d-flex flex-column align-items-center">
+                <img src="https://www.sunlight.co.id/images/h0nadbhvm6m4/1VLa8YgpNnTTHS8hVcRpFx/f877e731e3474ef8f0b991083e3d69a1/U3VubGlnaHRfSmVydWtfTmlwaXMxLnBuZw/1080w-1080h/sunlight-jeruk-nipis.jpg" alt="">
+                <div class="title-produk">sunlight</div>
+                <div class="price">Rp 21.500</div>
+                </a>
+                <button class="btn cart-btn"><span class="material-symbols-outlined">
+                    add
+                    </span>Keranjang</button>
+            </div>
+            <div class="card-list d-flex flex-column align-items-center">
+                <img src="https://www.sunlight.co.id/images/h0nadbhvm6m4/1VLa8YgpNnTTHS8hVcRpFx/f877e731e3474ef8f0b991083e3d69a1/U3VubGlnaHRfSmVydWtfTmlwaXMxLnBuZw/1080w-1080h/sunlight-jeruk-nipis.jpg" alt="">
+                <div class="title-produk">sunlight</div>
+                <div class="price">Rp 21.500</div>
+                </a>
+                <button class="btn cart-btn"><span class="material-symbols-outlined">
+                    add
+                    </span>Keranjang</button>
+            </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        function w3_open() {
+          document.getElementById("mySidebar").style.display = "block";
+        }
+
+        function w3_close() {
+          document.getElementById("mySidebar").style.display = "none";
+        }
+        </script>
+@endsection
+
--- resources/views/fronsite/layouts/navbar.blade.php
+++ resources/views/fronsite/layouts/navbar.blade.php
@@ -55,6 +55,7 @@
       </nav>
 
       @yield('content')
+      @yield('food-content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" integrity="sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
